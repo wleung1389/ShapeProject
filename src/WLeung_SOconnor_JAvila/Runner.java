48b883aaff49ddcbbc19205d8d1e4f6b6363bb1b
BT_1N3_29 <wleung1389@bths.edu>
2018-10-02 09:44:37 -0400

    Hi

---
+++ src/WLeung_SOconnor_JAvila/Runner.java
@@ -0,0 +1,24 @@
+package WLeung_SOconnor_JAvila;
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        Shape shape1 = new Rectangle(5,10);
+        Shape shape2 = new ETriangle(5);
+        Shape shape3 = new Circle(10);
+        Shape shape4 = new RPentagon(5);
+        System.out.println(shape4);
+        System.out.println("My Perimeter is : " + shape4.getPerimeter());
+        System.out.println("My Area is : " +  shape4.getArea());
+        Shape[] shapes {shape1, shape2, shape3};
+
+        for(Shape cur : shapes)
+        {
+            System.out.println(cur);
+            System.out.println("My Perimeter is : " + cur.getArea());
+            System.out.println("My Area is : "  + cur.getArea());
+        }
+
+    }
+}
